33c64f9db82565462bae7f4c6111ba5c45bb7c94
gptjd0204 <gptjd0204@naver.com>
2026-06-05 11:45:09 +0900

    chore: seed.js 설명 주석 추가

--- prisma/seeds/seed.js
+++ prisma/seeds/seed.js
@@ -2,13 +2,18 @@ import { PrismaClient } from '@prisma/client';
 import bcrypt from 'bcrypt';
 const prisma = new PrismaClient();
 
+/*
+  1. 시드 데이터 코드 작성
+  2. npm run seed
+*/
+
 async function main() {
   // 기존 데이터 정리
   // await prisma.user.deleteMany();
 
-  // user seedData 생성
+  // user seedData 생성 예시
   // await prisma.user.createMany({
-  //   data: ,
+  //   data: 데이터 ,
   // });
 
   console.log('시드 데이터 입력 완료!');
