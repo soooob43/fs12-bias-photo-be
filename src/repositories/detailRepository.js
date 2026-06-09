6466dd283fa3eaba00f02b1bbb51378ac28f7fda
신영미 <sin-yeongmi@MacBook-Air.local>
2026-06-09 16:17:11 +0900

    fix: 민감정보 불러오기 제외 및 공통 에러핸들러 반영

--- src/repositories/detailRepository.js
+++ src/repositories/detailRepository.js
@@ -6,9 +6,30 @@ const getMarketDetail = async (transactionId) => {
       id: Number(transactionId),
       isDeleted: false, // 삭제되지 않은 판매정보만 불러오기
     },
-    include: {
+    select: {
+      // transaction 필요 정보
+      id: true,
+      sellerId: true,
+      price: true,
+      totalQuantity: true,
+      remainingQuantity: true,
+      exchangeGrade: true,
+      exchangeGenre: true,
+      exchangeDescription: true,
+      createdAt: true,
+      updatedAt: true,
+      isDeleted: true,
+
+      // card 테이블 정보 모두
       card: true,
-      seller: true,
+
+      // seller id, nickname 만
+      seller: {
+        select: {
+          id: true,
+          nickname: true,
+        },
+      },
     },
   });
 };
--- src/services/detailService.js
+++ src/services/detailService.js
@@ -1,11 +1,16 @@
 import detailRepository from '../repositories/detailRepository.js';
+import AppError from '../utils/appError.js'; // 공통 에러 핸들러 추가
 
 const getPhotocard = async (transactionId) => {
   const transaction = await detailRepository.getMarketDetail(transactionId);
 
   //해당 ID 포토카드 판매 정보가 없는 경우
   if (!transaction) {
-    throw new Error('해당 포토카드의 판매 정보를 찾을 수 없습니다.');
+    throw AppError(
+      404,
+      'TRANSACTION_NOT_FOUND',
+      '해당 포토카드의 판매 정보를 찾을 수 없습니다.',
+    );
   }
 
   return transaction;
