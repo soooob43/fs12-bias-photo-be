10d0ea8ec8fbd1abf37fc2f3e4fde75846856a0f
gptjd0204 <gptjd0204@naver.com>
2026-06-17 14:12:00 +0900

    chore: 카드 유효성 검사 수정

--- src/schemas/card.schema.js
+++ src/schemas/card.schema.js
@@ -10,7 +10,11 @@ export const cardSchema = z.object({
   imageUrl: z
     .string({ required_error: '이미지 URL이 필요합니다.' })
     .url('유효한 이미지 URL 형식이 아닙니다.'),
-  description: z.string().max(255, '설명은 255자 이하여야 합니다.').optional(),
+  description: z
+    .string()
+    .min(1, '포토 카드 설명을 입력해주세요.')
+    .max(255, '설명은 255자 이하여야 합니다.')
+    .optional(),
   grade: z.nativeEnum(CardGrade, {
     errorMap: () => ({ message: '유효하지 않은 카드 등급입니다.' }),
   }),
@@ -20,7 +24,8 @@ export const cardSchema = z.object({
   minimumPrice: z
     .number({ invalid_type_error: '최소 가격은 숫자여야 합니다.' })
     .int('소수점은 입력할 수 없습니다.')
-    .min(1, '최소 가격은 1P 이상이어야 합니다.'),
+    .min(1, '최소 가격은 1P 이상이어야 합니다.')
+    .max(1000000000, '가격은 1000000000P 이하여야 합니다.'),
   totalQuantity: z
     .number({ invalid_type_error: '총 발행량은 숫자여야 합니다.' })
     .int('소수점은 입력할 수 없습니다.')
